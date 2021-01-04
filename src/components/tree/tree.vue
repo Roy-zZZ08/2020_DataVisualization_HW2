38e57ddc7a795ee3fb3b735d75b1d91c0d6c9450
Lyz <978387805@qq.com>
2021-01-04 09:51:14 +0800

    enter

--- src/components/tree/tree.vue
+++ src/components/tree/tree.vue
@@ -70,75 +70,71 @@ export default {
 
       // 选择svg容器
       d3.select('#tree-container')
-        .style('width', '960rem')
-        .style('height', '960rem');
+        .style('width', '100rem')
+        .style('height', '100rem');
 
-      // 添加base svg
-      this.svg = d3
-        .select('#tree-container')
+      // 添加svg
+      this.svg = d3.select('#tree-container')
         .append('svg')
-        .attr('style', 'background: white')
-        .attr('width', '960rem')
-        .attr('height', '960rem')
-        .call(
-          d3.zoom().on('zoom', () => {
+        .attr('width', '90%')
+        .attr('height', '90%')
+        .attr('style', 'background: #fff')
+        .call(d3.zoom().on('zoom', () => {
             this.svg.attr('transform', d3.event.transform);
           })
-        )
-        .append('g');
+        ).append('g');
 
-      // 添加links svg
-      this.gLink = this.svg
-        .append('g')
+      // 添加links 
+      this.gLink = this.svg.append('g')
         .attr('fill', 'none')
         .attr('stroke', '#555')
         .attr('stroke-opacity', 0.4)
         .attr('stroke-width', 1.5);
-      // 添加nodes svg
-      this.gNode = this.svg
-        .append('g')
+
+      // 添加nodes 
+      this.gNode = this.svg.append('g')
         .attr('cursor', 'pointer')
         .attr('pointer-events', 'all');
+
+
       // load data
-      this.treeRoot = d3.hierarchy(my_data);
-      this.treeRoot.x0 = this.height / 2;
-      this.treeRoot.y0 = 0;
-      this.treeRoot.descendants().forEach((d, i) => {
+      this.Root = d3.hierarchy(my_data);
+      this.Root.x0 = this.height / 2;
+      this.Root.y0 = 0;
+      this.Root.descendants().forEach((d, i) => {
         d.id = i;
         d._children = d.children;
-        // TODO initial to make some nodes closed
-        // if (d.depth && d.data.name.length !== 7) d.children = null;
       });
+      
       // 初始化tree
       this.tree = d3.tree();
-      // TODO adjust by the node size
-      this.tree.nodeSize([18, this.height]);
-      this.tree(this.treeRoot);
-      // this.tree = d3.tree(this.treeRoot).nodeSize([10, 25]);
-      const linkH = d3
+
+      // adjust by the node size
+      this.tree.nodeSize([20, 250]);
+
+      this.tree(this.Root);
+
+      const diagonal = d3
         .linkHorizontal()
         .x((d) => d.y)
         .y((d) => d.x);
-      /**
-       * @description: update tree layout when needed
-       * @param {*} source
-       * @return {*}
-       */
+
 
       const update = (source) => {
         const duration = d3.event && d3.event.altKey ? 2500 : 250;
-        const nodes = this.treeRoot.descendants().reverse();
-        const links = this.treeRoot.links();
+        const nodes = this.Root.descendants().reverse();
+        const links = this.Root.links();
         // Compute the new tree layout.
-        let left = this.treeRoot;
-        let right = this.treeRoot;
-        this.treeRoot.eachBefore((node) => {
+        this.tree(this.Root);
+        let left = this.Root;
+        let right = this.Root;
+        this.Root.eachBefore((node) => {
           if (node.x < left.x) left = node;
           if (node.x > right.x) right = node;
         });
         const height = right.x - left.x + this.margin.top + this.margin.bottom;
+
         // define transition
-        // TODO transition
         const transition = this.svg
           .transition()
           .duration(duration)
@@ -159,9 +155,7 @@ export default {
         const node = this.gNode.selectAll('g').data(nodes, (d) => d.id);
 
         // Enter any new nodes at the parent's previous position.
-        const nodeEnter = node
-          .enter()
-          .append('g')
+        const nodeEnter = node.enter().append('g')
           .attr('transform', () => `translate(${source.y0},${source.x0})`)
           .attr('fill-opacity', 0)
           .attr('stroke-opacity', 0)
@@ -171,8 +165,7 @@ export default {
             update(d);
           });
 
-        nodeEnter
-          .append('circle')
+        nodeEnter.append('circle')
           .attr('class', 'nodeCircle')
           .attr('r', 2.5)
           .attr('fill', (d) => (d._children ? '#555' : '#999'))
@@ -193,13 +186,12 @@ export default {
 
         // Transition nodes to their new position.
 
-        const nodeUpdate = node
-          .merge(nodeEnter)
+        const nodeUpdate = node.merge(nodeEnter)
           .transition(transition)
           .attr('transform', (d) => `translate(${d.y},${d.x})`)
           .attr('fill-opacity', 1)
           .attr('stroke-opacity', 1);
-        console.log(nodeUpdate);
+       
 
         // Transition exiting nodes to the parent's new position.
         const nodeExit = node
@@ -209,26 +201,26 @@ export default {
           .attr('transform', `translate(${source.y},${source.x})`)
           .attr('fill-opacity', 0)
           .attr('stroke-opacity', 0);
-        console.log(nodeExit);
 
         // update the links...
         const link = this.gLink
           .selectAll('path')
           .data(links, (d) => d.target.id);
+
         // Enter any new links at the parent's previous position.
         const linkEnter = link
           .enter()
           .append('path')
           .attr('d', () => {
             const o = { x: source.x0, y: source.y0 };
-            return linkH({ source: o, target: o });
+            return diagonal({ source: o, target: o });
           });
 
         // Transition links to their new position.
         link
           .merge(linkEnter)
           .transition(transition)
-          .attr('d', linkH);
+          .attr('d', diagonal);
 
         // Transition exiting nodes to the parent's new position.
         link
@@ -237,21 +229,21 @@ export default {
           .remove()
           .attr('d', () => {
             const o = { x: source.x, y: source.y };
-            return linkH({ source: o, target: o });
+            return diagonal({ source: o, target: o });
           });
         // Stash the old positions for transition.
         nodes.forEach((d) => {
           d.x0 = d.x;
           d.y0 = d.y;
         });
-      };
-      /**
-       * @description: collapse or expand and centered tree when node is clicked
-       * @param {*} d
-       * @return {*}
-       */
 
-      update(this.treeRoot);
+        console.log(nodeUpdate);
+        console.log(nodeExit);
+      };
+
+      update(this.Root);
+
+      
 
       // 添加图表标题
       this.title = this.svg
